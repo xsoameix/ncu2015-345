822465cca55c4ab9a90c29d12577dadc2248951e
HelloRose <parischen1993@hotmail.com>
2015-12-31 22:32:48 +0800

    New Model Branch
    Add Model.java
    
    Update ClientModel
    public void requestFire()
    public void requestStartGame()
    public void requestSetTotalTime(int time)
    public void requestSetPlayerNumber(int time)
    public void requestSetLocation(int x, int y)
    public void requestKeyInput(int key)
    public Boolean requestEstablishRoom(int port)
    public Boolean requestEnterRoom(String ip, int port)
    public Model getModel()

---
+++ src/model/Model.java
@@ -0,0 +1,146 @@
+package model;
+
+import model.game.field.Map;
+import model.game.field.dynamic.Bullet;
+import model.game.field.dynamic.Obstacle;
+import model.setting.Profile;
+
+public class Model {
+	// server side
+	// private ServerModel serverModel
+
+	// client side
+	// private TCPClient tcpClient;
+	// private UDPServer udpServer;
+	private Setting setting;
+	private Room room;
+	private Game game;
+	private Profile profile;
+
+	/* for client model start */
+	// host
+	protected boolean requestEstablishRoom(int port) {
+		// room=new Room();
+		/*
+		 * tcpServer=new TCPServer();
+		 * 
+		 * 
+		 * enterRoom();
+		 */
+		return true;
+	}
+
+	// client
+	protected boolean requestEnterRoom(String ip, int port) {
+		// room=new Room();//get room from server? or get data from server and
+		// set to new room?
+		// Player player=new Player();
+		// player.setProfile(setting.getProfile());
+		// room.addPlayer(player);
+		/*
+		 * tcpClient=new TCPClient();
+		 */
+		return true;
+	}
+
+	protected void requestFire() {
+
+	}
+
+	protected void requestStartGame() {
+
+	}
+
+	protected void requestSetTotalTime(int time) {
+
+	}
+
+	protected void requestSetPlayerNumber(int time) {
+
+	}
+
+	protected void requestSetLocation(int x, int y) {
+
+	}
+
+	protected void requestKeyInput(int key) {
+
+	}
+
+	/* for client model end */
+
+	/* for UDP start */
+	public void setTotalTime(int second) {
+
+	}
+
+	public void setPlayerNumber(int playnumber) {
+
+	}
+
+	public boolean addPlayer(Profile profile) {
+
+	}
+
+	public boolean removePlayer(Profile profile) {
+
+	}
+
+	public void gameOver(Result result) {
+
+	}
+
+	public void setTime(int second) {
+
+	}
+
+	public boolean addBullet(Bullet bullet) {
+
+	}
+
+	public boolean removeBullet(Bullet bullet) {
+
+	}
+
+	public boolean removeObstacle(Obstacle obstale) {
+
+	}
+
+	public void setMap(Map map) {
+
+	}
+
+	public void set(Byte[] packet) {
+
+	}
+
+	public void startGame() {
+
+	}
+
+	public void setLocation(int x, int y) {
+
+	}
+
+	/* for UDP end */
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public Setting getSetting() {
+		return setting;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public int getTime() {
+		return -1;
+	}
+}

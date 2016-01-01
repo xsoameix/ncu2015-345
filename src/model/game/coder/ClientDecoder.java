854bf1cdc6077426181d69a86fa4d21aa647103f
Lion <desertblackeagle@gmail.com>
2016-01-02 02:01:51 +0800

    Update En/Decoder

--- src/model/game/coder/ClientDecoder.java
+++ src/model/game/coder/ClientDecoder.java
@@ -1,9 +1,14 @@
 package model.game.coder;
 
 import java.util.Iterator;
+import java.util.Vector;
 
 import model.Model;
 import model.game.Player;
+import model.game.Result;
+import model.game.Team;
+import model.game.field.dynamic.Bullet;
+import model.game.field.dynamic.Obstacle;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,70 +26,75 @@ public class ClientDecoder {
 		Iterator<?> key = object.keys();
 		while (key.hasNext()) {
 			String keys = (String) key.next();
-			switch (keys) {
-			case "removeObstacle":
-				//
-				break;
-			case "setTotalTime":
-				try {
+			try {
+				switch (keys) {
+				case "removeObstacle":
+					Obstacle obstacle = (Obstacle) object.get(keys);
+					model.removeObstacle(obstacle);
+					break;
+				case "setTotalTime":
 					int totalTime = object.getInt(keys);
 					model.setTotalTime(totalTime);
-				} catch (JSONException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				break;
-			case "setTime":
-				try {
+					break;
+				case "setTime":
 					int time = object.getInt(keys);
 					model.setTime(time);
-				} catch (JSONException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				break;
-			case "setPlayerNumber":
-				//
-				break;
-			case "addPlayer":
-				//
-				break;
-			case "removePlayer":
-				break;
-			case "setLocation":
-				Player player = null;
-				try {
+					break;
+				case "setPlayerNumber":
+					int playerNumber = object.getInt(keys);
+					model.setPlayerNumber(playerNumber);
+					break;
+				case "addPlayer":
+					Player player = (Player) object.get(keys);
+					model.addPlayer(player);
+					break;
+				case "removePlayer":
+					player = null;
 					player = (Player) object.get(keys);
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					model.removePlayer(player);
+					break;
+				case "setLocation":
+					player = null;
+					player = (Player) object.get(keys);
+					model.setLocation(player);
+					break;
+				case "setMoney":
+					Vector<Team> teams = (Vector<Team>) object.get(keys);
+					model.setMoney(teams);
+					break;
+				case "setKillNumber":
+					player = null;
+					player = (Player) object.get(keys);
+					model.setKillNumber(player);
+					break;
+				case "addBullet":
+					Bullet bullet = (Bullet) object.get(keys);
+					model.addBullet(bullet);
+					break;
+				case "removeBullet":
+					bullet = null;
+					bullet = (Bullet) object.get(keys);
+					model.removeBullet(bullet);
+					break;
+				case "updateBullet":
+					bullet = null;
+					bullet = (Bullet) object.get(keys);
+					model.updateBullet(bullet);
+					break;
+				case "changeFlagColor":
+					//
+					break;
+				case "startGame":
+					model.startGame();
+					break;
+				case "gameOver":
+					Result result = (Result) object.get(keys);
+					model.gameOver(result);
+					break;
 				}
-				model.setLocation(player);
-				break;
-			case "setMoney":
-				//
-				break;
-			case "setKillNumber":
-				//
-				break;
-			case "addBullet":
-				//
-				break;
-			case "removeBullet":
-				//
-				break;
-			case "updateBullet":
-				//
-				break;
-			case "changeFlagColor":
-				//
-				break;
-			case "startGame":
-				// call ui to start game
-				break;
-			case "gameOver":
-				//
-				break;
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
 		}
 	}

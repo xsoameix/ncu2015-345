b83a9d97188bd5a29528d0ba4c448fa8936ae31b
Lion <desertblackeagle@gmail.com>
2016-01-01 18:20:51 +0800

    Update Client/Server De/Encoder

--- src/model/game/coder/ClientEncoder.java
+++ src/model/game/coder/ClientEncoder.java
@@ -8,30 +8,37 @@ import org.json.JSONObject;
 import model.game.Player;
 
 public class ClientEncoder {
-	//in room
-	public JSONObject requestSetGameTime(int totalTime){
-		return encodeObject("requestSetGameTime", totalTime);
+	// in room
+	public JSONObject requestSetTotalTime(int totalTime) {
+		return encodeObject("requestSetTotalTime", totalTime);
 	}
-	public JSONObject requestSetPlayerNumber(int playerNumber){
+
+	public JSONObject requestSetPlayerNumber(int playerNumber) {
 		return encodeObject("requestSetPlayerNumber", playerNumber);
 	}
-	public JSONObject requestAddPlayer(Player player){
+
+	public JSONObject requestAddPlayer(Player player) {
 		return encodeObject("requestAddPlayer", player);
 	}
-	public JSONObject requestRemovePlayer(Player player){
+
+	public JSONObject requestRemovePlayer(Player player) {
 		return encodeObject("requestRemovePlayer", player);
 	}
-	public JSONObject requestStartGame(){
+
+	public JSONObject requestStartGame() {
 		return encodeObject("requestStartGame", null);
 	}
-	public JSONObject requestSetLocation(Point point){
+
+	public JSONObject requestSetLocation(Point point) {
 		return encodeObject("requestSetLocation", point);
 	}
-	public JSONObject requestFire(){
+
+	public JSONObject requestFire() {
 		return encodeObject("requestFire", null);
 	}
-	public JSONObject encodeObject(String key, Object value){
-		JSONObject object=new JSONObject();
+
+	public JSONObject encodeObject(String key, Object value) {
+		JSONObject object = new JSONObject();
 		try {
 			object.put(key, value);
 		} catch (JSONException e) {

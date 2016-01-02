890e123a8d2eb6afe757401d0613519e517a493a
Mango-angel <literary-sketch@163.com>
2016-01-02 16:43:08 +0800

    Updata Rule-collusioon & tankMovingCheck

--- src/model/game/Rule.java
+++ src/model/game/Rule.java
@@ -1,5 +1,102 @@
 package model.game;
 
-public class Rule {
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Vector;
 
+import model.Room;
+import model.game.field.FieldObject;
+import model.game.field.Map;
+import model.game.field.dynamic.Bullet;
+import model.game.field.dynamic.Character;
+import model.game.field.map.MapBlock;
+
+public class Rule {
+	
+	private Map map;
+	private Field field;
+	private Room room;
+	//private Vector<DynamicObject> List;
+	
+	public Rule(Map map, Field field, Room room){
+		this.map = map;
+		this.field = field;
+		this.room = room;
+	}
+	
+	public boolean tankMovingCheck(FieldObject current){
+		if(IsCollusion(current) == null)
+			return true;
+		else
+			return false;
+	}
+	
+	public FieldObject IsCollusion(FieldObject current){
+		// = List.iterator();
+		Iterator<FieldObject> iter;
+		Rectangle CurrentRectangle = null;
+		
+		//get current object's rectangle
+		//Bullet's rectangle smaller than other field object
+		if(current instanceof Bullet){
+			CurrentRectangle = ((Bullet)current).getRectangle();
+		}
+		else
+			CurrentRectangle = current.getRectangle();
+		
+		//MapBlocks which is nearby the current 
+		int posX = current.getLocation().x/MapBlock.getDimension().width;
+		int posy = current.getLocation().y/MapBlock.getDimension().height;
+		
+		for(int i = posy-1; i <= posy+1; i++){
+			for(int j = posX-1; j <= posX+1; j++)
+			{
+				if(i!=posy && j!=posX)
+					//MapBlock has nothing(just a background)
+					if(! map.getMapBlock(i, j).getDynamicObjectList().isEmpty());
+					{
+						iter = map.getMapBlock(i, j).getDynamicObjectList().iterator();
+						while( iter.hasNext()){
+							//current crash with MapBlock's dynamicObjects
+							FieldObject fieldObject = iter.next();
+							//Bullet's rectangle smaller than other field object
+							if(fieldObject instanceof Bullet){
+								if(((Bullet)fieldObject).getRectangle().intersects(CurrentRectangle)){
+									return (FieldObject)iter;
+								}
+							}
+							else if(fieldObject.getRectangle().intersects(CurrentRectangle)){
+								return (FieldObject)iter;
+							}
+								
+						}
+					}
+			}
+		}
+		return null;
+	}
+	
+	public void TankHitTank(){
+		
+	}
+	
+	public void TankHitObstacle(){
+		
+	}
+	
+	public void BulletHitTank(Bullet bullet, Character character){
+	
+	}
+	
+	public void BulletHitObstacle(){
+		
+	}
+	
+	public void BulletHitBullet(){
+		
+	}
+	
+	
 }
--- src/model/game/field/dynamic/Bullet.java
+++ src/model/game/field/dynamic/Bullet.java
@@ -1,6 +1,9 @@
 package model.game.field.dynamic;
 
+import java.awt.Rectangle;
+
 import model.game.field.FieldObject;
+import model.game.field.map.MapBlock;
 
 public class Bullet extends FieldObject {
 	private int direction;
@@ -11,6 +14,15 @@ public class Bullet extends FieldObject {
 
 	public void setDirection(int dir) {
 		this.direction = dir;
+	}
+	
+	@Override
+	public Rectangle getRectangle(){
+		Rectangle rect = new Rectangle(this.getLocation().x/MapBlock.getDimension().width,
+									   this.getLocation().x/MapBlock.getDimension().height,
+				                       30,
+				                       10);
+		return rect;
 	}
 
 }
